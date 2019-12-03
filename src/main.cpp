69c730c3b882be62bb1e813301b884c941a78065
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2019-12-03 15:54:46 +0100

    Oneline temperature sensor code

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,17 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+//Temperature sensor setup
+#define ONE_WIRE_BUS 4
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 
 const char *ssid =  "Stads-Lab";     // replace with your wifi ssid and wpa2 key
 const char *pass =  "initialkey4iot";
 const char *mqtt_server = "plantacle.com";
-
-//Thermistor settings
-const double VCC = 3.3;             // NodeMCU on board 3.3v vcc
-const double R2 = 10000;            // 10k ohm series resistor
-const double adc_resolution = 1023; // 10-bit adc
-
-//Thermistor Equition Parameters
-const double A = 0.001129148;   
-const double B = 0.000234125;
-const double C = 0.0000000876741; 
-
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -45,7 +40,7 @@ void setup()
   client.connect("whatever", "plantacle", "123456789");
   Serial.println(client.state());  
 
-
+  sensors.begin();
 }
 
 void loop()
@@ -58,15 +53,9 @@ void loop()
   Serial.println(humidity);
   Serial.println("Temperature: ");
 
-
-    //Thermistor
-  double Vout, Rth, temperature, adc_value; 
-  adc_value = analogRead(A0);
-  Vout = (adc_value * VCC) / adc_resolution;
-  Rth = (VCC * R2 / Vout) - R2;
-  temperature = (1 / (A + (B * log(Rth)) + (C * pow((log(Rth)),3))));   // Temperature in kelvin
-  temperature = temperature - 273.15;  // Temperature in degree celsius
-
+  sensors.requestTemperatures(); 
+  float temperature = sensors.getTempCByIndex(0);
+ 
   Serial.println(temperature);
   const char* topic = "/plantacle/example";
   const String json = "{\"temperature\": " + String(temperature, 2) + ", \"humidity\": " + String(humidity, 2) + " }";
